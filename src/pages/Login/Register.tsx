2b018139306179c9732593261d01bd7cb252b9ef
Thiago Henrique <thiagohcfidencio@gmail.com>
2023-08-18 17:46:55 -0300

    Creating register css

--- src/pages/Login/Register.tsx
+++ src/pages/Login/Register.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { createUserWithEmailAndPassword } from 'firebase/auth';//função para criar user com email e senha
 import { auth } from '../../config/firebase';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
-import { yupResolver } from '@hookform/resolvers/yup'
+import { yupResolver } from '@hookform/resolvers/yup';
+
+import './Register.css';
+import { HiOutlineMail } from 'react-icons/hi';
+import { RiLockPasswordLine } from 'react-icons/ri';
 
 const Register = () => {
 
   const schema = yup.object().shape({
     email: yup.string().email().required(),
     password: yup.string().required().min(6).max(20),
-    confirmPassword: yup.string().oneOf([yup.ref("password"), null]).required(),
+    confirmPassword: yup.string().oneOf([yup.ref('password'), null]).required(),
   });
 
   const {register, handleSubmit } = useForm({
@@ -29,25 +33,56 @@ const Register = () => {
   const onSubmit = data => createAcount(data);
  
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <h1>Register</h1>
-      <input 
-        type="email" 
-        placeholder="Your email..." 
-        {...register('email')}
-      />
-      <input 
-        type="password" 
-        placeholder="Your password..." 
-        {...register('password')}
-      />
-      <input 
-        type="password" 
-        placeholder="Confirm your password..." 
-        {...register('confirmPassword')}
-      />
-      <input type="submit"/>
-    </form>
+    <section className="login">
+      <div className="login-left">
+        <h1>Hello, Friend!</h1>
+        <p>Enter your personal details and start journey with us</p>
+      </div>
+
+      <div className="login-right">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <h1>Register</h1>
+          <label>
+            <span className="icon">
+              <HiOutlineMail size={25} color="#A084E8" />
+            </span>
+            <input 
+              className="reg-input"
+              type="email" 
+              placeholder="Email" 
+              {...register('email')}
+            />
+          </label>
+          <label>
+            <span className="icon">
+              <RiLockPasswordLine size={25} color="#A084E8" />
+            </span>
+            <input 
+              className="reg-input"
+              type="password" 
+              placeholder="Your password..." 
+              {...register('password')}
+            />
+          </label>
+          <label>
+            <span className="icon">
+              <RiLockPasswordLine size={25} color="#A084E8" />
+            </span>
+            <input 
+              className="reg-input"
+              type="password" 
+              placeholder="Confirm Password" 
+              {...register('confirmPassword')}
+            />
+          </label>
+          
+          <input className="login-btn" type="submit"/>
+          <div className="no-acount">
+            <Link to={'/login'} className="link">Back to Login Page.</Link>
+          </div>
+        </form>
+      </div>    
+    </section>
   );
 };
 
